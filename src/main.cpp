ac4217998b407f5d1272b9a5043eccff21e498f8
Michalis Ioak <ioakimidismichalis@gmail.com>
2024-12-06 21:39:50 +0200

    ff

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,169 @@
+#include "ac_float.h"
+#include "ac_int.h"
 #include "read_txt.h"
 #include "top.h"
 #include <iostream>
 
 #define A_SIZE 5
-#define H_ROWS 500
+#define H_COLS 500
 #define W_COLS 16
 
+template <typename T, int N> void create_random_graph(T A[N][N]) {
+  srand(time(0));
+  for (int i = 0; i < N; ++i) {
+    for (int j = i + 1; j < N; ++j) {
+      int edge = rand() % 2;
+      A[i][j] = edge;
+      A[j][i] = edge;
+    }
+    A[i][i] = 1;
+  }
+}
+
+template <typename T, typename I, int N>
+void compute_sqrt_degree_matrix(T A[N][N], I D[N][N]) {
+  for (int i = 0; i < N; ++i) {
+    T degree = 0.0;
+    for (int j = 0; j < N; ++j) {
+      degree += A[i][j];
+    }
+    for (int j = 0; j < N; ++j) {
+      D[i][j] = (i == j) ? 1 / sqrt(degree) : 0.0;
+    }
+  }
+}
+
+template <typename T, int N>
+void compute_normalized_adjacency_matrix(T A[N][N], T D[N][N],
+                                         T A_tilde[N][N]) {
+  T tmp[N][N];
+  for (int k = 0; k < A_SIZE; k++) {
+    for (int m = 0; m < A_SIZE; m++) {
+      for (int n = 0; n < A_SIZE; n++) {
+        tmp[m][n] += D[m][k] * A[k][n];
+      }
+    }
+  }
+
+  for (int k = 0; k < A_SIZE; k++) {
+    for (int m = 0; m < A_SIZE; m++) {
+      for (int n = 0; n < A_SIZE; n++) {
+        A_tilde[m][n] += tmp[m][k] * D[k][n];
+      }
+    }
+  }
+}
+
+template <typename T, int A, int B> void create_feature_matrix(T H_in[A][B]) {
+  srand(time(0));
+  for (int i = 0; i < A; ++i) {
+    for (int j = 0; j < B; ++j) {
+      H_in[i][j] = abs(rand() % 256);
+    }
+  }
+}
+
 int main() {
-  std::cout << "Init Testing...." << std::endl;
+  std::cout << "Init...." << std::endl;
+  double A[A_SIZE][A_SIZE] = {{1, 1, 1, 0, 0},
+                              {1, 1, 1, 1, 0},
+                              {1, 1, 1, 1, 1},
+                              {0, 1, 1, 1, 1},
+                              {0, 0, 1, 1, 1}};
+  double D[A_SIZE][A_SIZE];
+  double A_tilde[A_SIZE][A_SIZE];
+  double H_in[A_SIZE][H_COLS];
 
-  //   createRandomGraph(A);
-  //   computeDegreeMatrix(A, D);
-  //   computeNormalizedAdjacencyMatrix(A, D, A_tilde);
-  //   createFeatureMatrix(H_in);
+  std::cout << "Starting init data..." << std::endl;
+  // create_random_graph(A);
+  std::cout << "Graph (Adjacency Matrix):\n";
+  for (int i = 0; i < A_SIZE; ++i) {
+    for (int j = 0; j < A_SIZE; ++j) {
+      std::cout << A[i][j] << " ";
+    }
+    std::cout << "\n";
+  }
+  compute_sqrt_degree_matrix(A, D);
+  std::cout << "D (Adjacency Matrix):\n";
+  for (int i = 0; i < A_SIZE; ++i) {
+    for (int j = 0; j < A_SIZE; ++j) {
+      std::cout << D[i][j] << " ";
+    }
+    std::cout << "\n";
+  }
+  compute_normalized_adjacency_matrix(A, D, A_tilde);
+  std::cout << "Á_tilde (Adjacency Matrix):\n";
+  for (int i = 0; i < A_SIZE; ++i) {
+    for (int j = 0; j < A_SIZE; ++j) {
+      std::cout << A_tilde[i][j] << " ";
+    }
+    std::cout << "\n";
+  }
+  create_feature_matrix<double, A_SIZE, H_COLS>(H_in);
+  std::cout << "Finished init data" << std::endl;
 
-  //   calc<int, H_ROWS, H_COLS, W_COLS>(A, H_in, H_out_dut);
+  std::cout << "Starting calculating H_out..." << std::endl;
+  double W[H_COLS][W_COLS];
+  read_data<double, H_COLS, W_COLS>(W, "weights.txt");
+
+  double temp[A_SIZE][H_COLS]; // A_tilde * H_in
+  double H_out[A_SIZE][W_COLS];
+
+  for (int k = 0; k < A_SIZE; k++) {
+    for (int m = 0; m < A_SIZE; m++) {
+      for (int n = 0; n < H_COLS; n++) {
+        temp[m][n] += A_tilde[m][k] * H_in[k][n];
+      }
+    }
+  }
+  for (int k = 0; k < H_COLS; k++) {
+    for (int m = 0; m < A_SIZE; m++) {
+      for (int n = 0; n < H_COLS; n++) {
+        H_out[m][n] += temp[m][k] * W[k][n];
+      }
+    }
+  }
+  for (int i = 0; i < A_SIZE; i++) {
+    for (int j = 0; j < W_COLS; j++) {
+      H_out[i][j] = (H_out[i][j] > 0) ? H_out[i][j] : 0;
+    }
+  }
+  std::cout << "Finished calculating H_out" << std::endl;
+
+  std::cout << "Starting calculating ac_H_out..." << std::endl;
+  ac_fixed<16, 8, true> ac_A[A_SIZE][A_SIZE];
+  ac_fixed<16, 8, true> ac_H_out[A_SIZE][W_COLS];
+  ac_fixed<16, 8, true> ac_H_in[A_SIZE][H_COLS];
+
+  for (int i = 0; i < A_SIZE; i++) {
+    for (int j = 0; j < A_SIZE; j++) {
+      ac_A[i][j] = ac_fixed<16, 8, true>(A_tilde[i][j]);
+      std::cout << A_tilde[i][j] << " " << ac_A[i][j] << std::endl;
+    }
+  }
+  for (int i = 0; i < A_SIZE; i++) {
+    for (int j = 0; j < H_COLS; j++) {
+      ac_H_in[i][j] = ac_fixed<16, 8, true>(H_in[i][j]);
+      std::cout << H_in[i][j] << " " << ac_H_in[i][j] << std::endl;
+    }
+  }
+
+  calc(ac_A, ac_H_in, ac_H_out);
+  std::cout << "Finished calculating ac_H_out" << std::endl;
+
+  std::cout << "Starting testing..." << std::endl;
+  for (int i = 0; i < A_SIZE; i++) {
+    for (int j = 0; j < W_COLS; j++) {
+      if (ac_H_out[i][j] != H_out[i][j]) {
+        std::cout << "At [" << i << "," << j << "]" << std::endl;
+        std::cout << "Expected: " << H_out[i][j] << std::endl;
+        std::cout << "Got: " << ac_H_out[i][j] << std::endl;
+        return -1;
+      }
+    }
+  }
+
+  std::cout << "Finished Testing!!!" << std::endl;
 
   return 0;
 }
--- src/top.h
+++ src/top.h
@@ -1,24 +1,8 @@
 #include "read_txt.h"
-#include <ac_math/ac_relu.h>
 
 #pragma HLS target_frequency "500 MHz"
 
-template <typename T, int A_SIZE, int H_COLS, int W_COLS>
-void calc(T A[A_SIZE][A_SIZE], T H_in[A_SIZE][H_COLS],
-          T H_out[A_SIZE][W_COLS]) {
-  T weights[H_COLS][W_COLS] = {0};
-  read_data<T, H_COLS, W_COLS>(weights, "weights.txt");
-
-  T tmp1[A_SIZE][H_COLS] = {0};
-  multiply_matrices(A, H_in, tmp1);
-
-  T tmp2[A_SIZE][H_COLS] = {0};
-  multiply_matrices(tmp1, weights, tmp2);
-
-  relu(tmp2, H_out);
-}
-
-template <typename T, int M, int K, int N>
+template <typename T, int M, int K, int N, int ID>
 void multiply_matrices(T A[M][K], T B[K][N], T C[M][N]) {
   for (int k = 0; k < K; k++) {
 #pragma hls_unroll
@@ -33,7 +17,22 @@ void multiply_matrices(T A[M][K], T B[K][N], T C[M][N]) {
 template <typename T, int M, int N> void relu(T in[M][N], T out[M][N]) {
   for (int m = 0; m < M; m++) {
     for (int n = 0; n < N; n++) {
-      ac_math::ac_relu(in[m][n], out[m][n]);
+      out[m][n] = (in[m][n] > 0) ? in[m][n] : 0;
     }
   }
+}
+
+template <typename T, int A_SIZE, int H_COLS, int W_COLS>
+void calc(T A[A_SIZE][A_SIZE], T H_in[A_SIZE][H_COLS],
+          T H_out[A_SIZE][W_COLS]) {
+  T weights[H_COLS][W_COLS];
+  read_data<T, H_COLS, W_COLS>(weights, "weights.txt");
+
+  T tmp1[A_SIZE][H_COLS];
+  multiply_matrices<T, A_SIZE, A_SIZE, H_COLS, 0>(A, H_in, tmp1);
+
+  T tmp2[A_SIZE][W_COLS];
+  multiply_matrices<T, A_SIZE, H_COLS, W_COLS, 1>(tmp1, weights, tmp2);
+
+  relu<T, A_SIZE, W_COLS>(tmp2, H_out);
 }
